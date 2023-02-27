0512bb388bf7375e285c07bc273a272dc6af1e7c
Sean Smith <ranky26@gmail.com>
2023-02-27 11:40:47 +1000

    changing from vue3/vite to blazor wasm

---
+++ Models/ProductDetailsModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json.Serialization;
+
+namespace SouthPacificSeaUrchins.Models
+{
+    public class ProductDetailsModel
+    {
+        [JsonPropertyName("productName")]
+        public string Name { get; set; }
+        public string Availability { get; set; }
+        [JsonPropertyName("productDescription")]
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- src/models/ProductDetails.ts
+++
@@ -1,6 +0,0 @@
-export default interface ProductDetailsModel {
-    productName: string    
-    availability: string;
-    productDescription: string;
-    imageUrl: string;
-}
