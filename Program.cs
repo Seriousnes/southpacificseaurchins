4316fbf8d02143d9bf824cc322f5bc8e1662f8a6
Sean Smith <ranky26@gmail.com>
2023-03-05 20:05:46 +1000

    mediaquery for resizing and breakpoint-specific layouts

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using BlazorPro.BlazorSize;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IWeatherService, WeatherService>();
+builder.Services.AddMediaQueryService();
 
 await builder.Build().RunAsync();
